3bcfd40c1ea689e4fb50deb0ba324d7e7be366c6
salamint <salamint@proton.me>
2025-07-27 19:59:31 +0200

    Created a solution, project and .gitignore

---
+++ BlasII.StylePoints/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+using BlasII.StylePoints;
+using MelonLoader;
+
+[assembly: MelonInfo(typeof(BlasII.StylePoints.Main), ModInfo.MOD_NAME, ModInfo.MOD_VERSION, ModInfo.MOD_AUTHOR)]
+[assembly: MelonOptionalDependencies("Modding API", "0.1.0")]
---
+++ BlasII.StylePoints/StylePoints.cs
@@ -0,0 +1,13 @@
+﻿using BlasII.ModdingAPI;
+
+namespace BlasII.StylePoints;
+
+public class StylePoints : BlasIIMod
+{
+    internal StylePoints() : base(ModInfo.MOD_ID, ModInfo.MOD_NAME, ModInfo.MOD_AUTHOR, ModInfo.MOD_VERSION) { }
+
+    protected override void OnInitialize()
+    {
+        // Perform initialization here
+    }
+}
