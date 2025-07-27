4545dc883fab51d41586f42aa5799bf731b3fa11
salamint <salamint@proton.me>
2025-07-27 19:59:31 +0200

    Created a solution, project and .gitignore

---
+++ BlasII.StylePoints/Main.cs
@@ -0,0 +1,13 @@
+﻿using MelonLoader;
+
+namespace BlasII.StylePoints;
+
+internal class Main : MelonMod
+{
+    public static StylePoints StylePoints { get; private set; }
+
+    public override void OnLateInitializeMelon()
+    {
+        StylePoints = new StylePoints();
+    }
+}
