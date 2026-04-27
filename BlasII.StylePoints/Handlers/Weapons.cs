8701b5ff8ecb9e68ecf638fe34a53dbb34c8a3f7
salamint <salamint@proton.me>
2026-04-27 12:53:15 +0200

    handlers: Incrementing the combo streak when hitting an enemy

---
+++ BlasII.StylePoints/Handlers/Weapons.cs
@@ -0,0 +1,13 @@
+using BlasII.Framework.WeaponEvents.Events;
+using Il2CppTGK.Game.Components.Attack.Data;
+
+namespace BlasII.StylePoints.Handlers;
+
+public class GeneralHandler : WeaponHandler
+{
+    public override void OnAttackHit(AttackInfo info)
+    {
+		Main.StylePoints.ComboMeter.Increment();
+    }
+}
+
