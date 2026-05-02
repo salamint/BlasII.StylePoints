522c65b5c4b1169ebff70e8e66d17196f05a7a16
salamint <salamint@proton.me>
2026-05-02 10:46:35 +0200

    main: Instantiated ComboMeter

--- BlasII.StylePoints/StylePoints.cs
+++ BlasII.StylePoints/StylePoints.cs
@@ -1,15 +1,18 @@
-﻿using BlasII.ModdingAPI;
+using BlasII.ModdingAPI;
+using BlasII.StylePoints.Combo;
 
 namespace BlasII.StylePoints;
 
 public class StylePoints : BlasIIMod
 {
-
     internal StylePoints() : base(ModInfo.MOD_ID, ModInfo.MOD_NAME, ModInfo.MOD_AUTHOR, ModInfo.MOD_VERSION) { }
 
-    protected override void OnInitialize()
+	private int currentRatingIndex = 0;
+
+	public ComboMeter ComboMeter = new ();
+
+    protected override void OnUpdate()
     {
-        // Perform initialization here
-		ModLog.Debug("Miracle May Cry initialized!");
+		ComboMeter.Update();
     }
 }
