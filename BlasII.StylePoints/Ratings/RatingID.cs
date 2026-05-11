d12e80baf5d2db6392484664bead7cf23e9e9938
salamint <salamint@proton.me>
2026-05-11 15:47:10 +0200

    ratings: Created the RatingID enum type to uniquely identify each rating

---
+++ BlasII.StylePoints/Ratings/RatingID.cs
@@ -0,0 +1,43 @@
+namespace BlasII.StylePoints.Ratings;
+
+/// <summary>
+/// Uniquely identies a different rating.
+/// </summary>
+public enum RatingID
+{
+	/// <summary>
+	/// Lowest rating.
+	/// </summary>
+	D,
+
+	/// <summary>
+	/// Second lowest rating.
+	/// </summary>
+	C,
+
+	/// <summary>
+	/// Third lowest rating.
+	/// </summary>
+	B,
+
+	/// <summary>
+	/// Average rating.
+	/// </summary>
+	A,
+
+	/// <summary>
+	/// Third highest rating.
+	/// </summary>
+	S,
+
+	/// <summary>
+	/// Second highest rating.
+	/// </summary>
+	SS,
+
+	/// <summary>
+	/// Highest rating.
+	/// </summary>
+	SSS
+}
+
