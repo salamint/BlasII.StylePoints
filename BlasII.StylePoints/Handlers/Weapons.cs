5e0f83a8f38a9c0a16c1e0a107382d841d3b5b86
salamint <salamint@proton.me>
2026-05-14 13:22:57 +0200

    combo: Added documentation

--- BlasII.StylePoints/Handlers/Weapons.cs
+++ BlasII.StylePoints/Handlers/Weapons.cs
@@ -3,13 +3,22 @@ using Il2CppTGK.Game.Components.Attack.Data;
 
 namespace BlasII.StylePoints.Handlers;
 
-public class GeneralHandler : WeaponHandler
+/// <summary>
+/// Weapon handler to update the combo meter.
+/// </summary>
+public class ComboHandler : WeaponHandler
 {
+	/// <summary>
+	/// Increments the combo meter when hitting an enemy.
+	/// </summary>
     public override void OnAttackHit(AttackInfo info)
     {
 		Main.StylePoints.ComboMeter.Increment();
     }
 
+	/// <summary>
+	/// Resetting the combo meter when resting at a Prie Dieu.
+	/// </summary>
     public override void OnRestAtPrieDieu()
     {
 		Main.StylePoints.ComboMeter.Reset();
