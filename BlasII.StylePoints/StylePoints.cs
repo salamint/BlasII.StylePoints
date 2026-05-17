6e9b2f19d65e46698f839bd422e0295f620f10b7
salamint <salamint@proton.me>
2026-05-17 14:06:31 +0200

    ratings: Created a RatingScore class

--- BlasII.StylePoints/StylePoints.cs
+++ BlasII.StylePoints/StylePoints.cs
@@ -1,9 +1,7 @@
-using System.Collections.Generic;
 using BlasII.ModdingAPI;
 using BlasII.StylePoints.Bonuses;
 using BlasII.StylePoints.Combo;
 using BlasII.StylePoints.Ratings;
-using UnityEngine;
 
 namespace BlasII.StylePoints;
 
@@ -11,73 +9,40 @@ public class StylePoints : BlasIIMod
 {
     internal StylePoints() : base(ModInfo.MOD_ID, ModInfo.MOD_NAME, ModInfo.MOD_AUTHOR, ModInfo.MOD_VERSION) { }
 
-	private RatingsManager RatingsManager { get; } = new ();
-	private List<RatingID> Ratings { get; } = new ()
-	{
-		RatingID.D,
-		RatingID.C,
-		RatingID.B,
-		RatingID.A,
-		RatingID.S,
-		RatingID.SS,
-		RatingID.SSS,
-	};
-
-	private int currentRatingIndex = 0;
-
-	public Rating CurrentRating { get => RatingsManager[Ratings[currentRatingIndex]]; }
-
 	public ComboMeter ComboMeter { get; } = new ();
 
 	public BonusesManager BonusesManager { get; } = new ();
 	public BonusQueue BonusQueue { get; } = new ();
 
-	private static string SwitchKeybinding = "SwitchStyleMeter";
+	public RatingsManager RatingsManager { get; } = new ();
+	public RatingScore RatingScore { get; } = new ();
 
 	public void AddBonus(BonusID id)
 	{
 		Bonus bonus = BonusesManager[id];
 		BonusQueue.Add(bonus);
+		RatingScore.Add(bonus);
 	}
 
     protected override void OnInitialize()
     {
         // Perform initialization here
-		InputHandler.RegisterDefaultKeybindings(new Dictionary<string, KeyCode>()
-		{
-			{ SwitchKeybinding, KeyCode.F8 },
-		});
-
 		RatingsManager.Init();
 		BonusesManager.Init();
     }
 
     protected override void OnSceneLoaded(string sceneName)
     {
-		CurrentRating.Text.Visible = true;
+		RatingScore.Visible = true;
     }
 
     protected override void OnSceneUnloaded(string sceneName)
     {
-		CurrentRating.Text.Visible = false;
+		RatingScore.Visible = false;
     }
 
     protected override void OnUpdate()
     {
-        if (InputHandler.GetKeyDown(SwitchKeybinding))
-		{
-			bool visible = CurrentRating.Text.Visible;
-			CurrentRating.Text.Visible = false;
-
-			currentRatingIndex++;
-			if (currentRatingIndex >= Ratings.Count)
-			{
-				currentRatingIndex = 0;
-			}
-
-			CurrentRating.Text.Visible = visible;
-		}
-
 		BonusQueue.Update();
 		ComboMeter.Update();
     }
