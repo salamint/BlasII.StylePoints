f126f6ad72a5e6f2886eb031955b0a23b10be46e
salamint <salamint@proton.me>
2026-05-19 20:30:55 +0200

    main: Added documentation to the main mod class

--- BlasII.StylePoints/StylePoints.cs
+++ BlasII.StylePoints/StylePoints.cs
@@ -5,18 +5,57 @@ using BlasII.StylePoints.Ratings;
 
 namespace BlasII.StylePoints;
 
+/// <summary>
+/// Class managing the Miracle May Cry mod, also called Style Points mod.
+/// It adds a style system to the game, as well as ratings and bonuses, to
+/// encourage the player to have a more varied and "stylish" playstyle.
+/// </summary>
 public class StylePoints : BlasIIMod
 {
-    internal StylePoints() : base(ModInfo.MOD_ID, ModInfo.MOD_NAME, ModInfo.MOD_AUTHOR, ModInfo.MOD_VERSION) { }
+	/* Properties */
 
+	/// <summary>
+	/// The combo meter keeps track of the combo of the player.
+	/// In other words, the number of hits the player inflicted to enemies
+	/// in a streak, without taking damage and before it expires.
+	/// </summary>
 	public ComboMeter ComboMeter { get; } = new ();
 
+	/// <summary>
+	/// The bonuses manager initializes the bonuses at a later stage once all of
+	/// the other components are initialized.
+	/// </summary>
 	public BonusesManager BonusesManager { get; } = new ();
+
+	/// <summary>
+	/// The bonus queue displays the last 5 bonus gained by the player during
+	/// their gameplay.
+	/// </summary>
 	public BonusQueue BonusQueue { get; } = new ();
 
+	/// <summary>
+	/// The ratings manager initializes the ratings at a later stage once all of
+	/// the other components are initialized.
+	/// </summary>
 	public RatingsManager RatingsManager { get; } = new ();
+
+	/// <summary>
+	/// The rating score keeps track of the global amount of style points
+	/// gained, decrease them with the time, and updates the current rating
+	/// depending on the current amount of style points.
+	/// </summary>
 	public RatingScore RatingScore { get; } = new ();
 
+	/* Constructors */
+
+    internal StylePoints() : base(ModInfo.MOD_ID, ModInfo.MOD_NAME, ModInfo.MOD_AUTHOR, ModInfo.MOD_VERSION) { }
+
+	/* Methods */
+
+	/// <summary>
+	/// Adds a bonus to the bonus queue and rating score, increasing the global
+	/// style score and being displayed at the bottom left of the screen.
+	/// </summary>
 	public void AddBonus(BonusID id)
 	{
 		Bonus bonus = BonusesManager[id];
@@ -24,26 +63,41 @@ public class StylePoints : BlasIIMod
 		RatingScore.Add(bonus);
 	}
 
+	/// <summary>
+	/// Initializes the mod at a late stage, once all mods are loaded by the mod
+	/// loader.
+	/// </summary>
     protected override void OnInitialize()
     {
-        // Perform initialization here
 		RatingsManager.Init();
 		BonusesManager.Init();
     }
 
+	/// <summary>
+	/// Called whenever the player is entering a room.
+	/// Shows the user interface.
+	/// </summary>
     protected override void OnSceneLoaded(string sceneName)
     {
 		RatingScore.Visible = true;
     }
 
+	/// <summary>
+	/// Called whenever the player is leaving a room.
+	/// Hides the user interface.
+	/// </summary>
     protected override void OnSceneUnloaded(string sceneName)
     {
 		RatingScore.Visible = false;
     }
 
+	/// <summary>
+	/// When an update is called, once per frame in the game loop.
+	/// Updates the bonus queue, combo meter and rating score.
+	/// </summary>
     protected override void OnUpdate()
     {
-		BonusQueue.Update();
-		ComboMeter.Update();
+		BonusQueue.Update(); ComboMeter.Update();
+		RatingScore.Update();
     }
 }
