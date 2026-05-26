ea5e24f729d949bc68843fd5b6a185a6d5284fb5
salamint <salamint@proton.me>
2026-05-26 12:42:31 +0200

    bonuses: Added bonuses for each weapon attacks

--- BlasII.StylePoints/Bonuses/BonusID.cs
+++ BlasII.StylePoints/Bonuses/BonusID.cs
@@ -5,5 +5,139 @@ namespace BlasII.StylePoints.Bonuses;
 /// </summary>
 public enum BonusID
 {
+	/* Common */
+
+	/// <summary>When killing a normal enemy</summary>
+	KILL,
+	/// <summary>When killing a big enemy</summary>
+	BIG_KILL,
+	/// <summary>
+	/// When inflicting a remarkable amount of damage to an enemy with low
+	/// health.
+	/// </summary>
+	OVER_KILL,
+	/// <summary>When executing a normal enemy</summary>
+	EXECUTION,
+	/// <summary>When executing a big enemy</summary>
+	BIG_EXECUTION,
+
+	/* Ruego al Alba attacks */
+
+	/// <summary>Default parry</summary>
+	BLADE_ROSARY_WARD,
+	/// <summary>Upgraded parry</summary>
+	BLADE_RETRIBUTION,
+
+	/// <summary>Default ground slam</summary>
+	BLADE_WEIGTH_OF_SIN,
+	/// <summary>Upgraded ground slam</summary>
+	BLADE_WEIGTH_OF_JUSTICE,
+
+	/// <summary>Ascending 3rd combo</summary>
+	BLADE_CRIMSON_ASCENSION,
+	/// <summary>Cyclone 3rd combo</summary>
+	BLADE_CRIMSON_CYCLONE,
+
+	/// <summary>4th combo</summary>
+	BLADE_CRIMSON_CLEAVER,
+	/// <summary>Upgraded 4th combo</summary>
+	BLADE_CRIMSON_GLAIVE,
+
+	/// <summary>Blood pact activation</summary>
+	BLADE_BLOOD_PACT,
+	/// <summary>Blood pact special attack</summary>
+	BLADE_REAPER_ROSARY,
+
+	/* Veredicto attacks */
+
+	/// <summary>Charged smash</summary>
+	CENSER_THUNDER_OF_MERCY,
+	/// <summary>Air spin charged attack</summary>
+	CENSER_AZURE_TYPHOON,
+	/// <summary>Charged attack shockwave</summary>
+	CENSER_AND_THE_EARTH_SHATTERED,
+	/// <summary>Upgraded charged attack shockwave</summary>
+	CENSER_SOUL_COMBUSTION,
+
+	/// <summary>Veredicto activation</summary>
+	CENSER_EMBERS_OF_FAITH,
+	/// <summary>Upgraded Veredicto activation</summary>
+	CENSER_EXPLOSIVE_EMBERS,
+
+	/// <summary>Activation combo</summary>
+	CENSER_IGNITION_STRIKE,
+	/// <summary>Activation mid-air</summary>
+	CENSER_AERIAL_IGNITION,
+	/// <summary>Loop combo</summary>
+	CENSER_ENDLESS_FLAME,
+
+	/* Sarmiento y Centella attacks */
+
+	/// <summary>Dash attack</summary>
+	RAPIER_LUNGE,
+	/// <summary>Upgraded dash attack</summary>
+	RAPIER_SILVER_LUNGE,
+
+	/// <summary>Default parry</summary>
+	RAPIER_CROSS_GUARD,
+	/// <summary>Upgraded parry</summary>
+	RAPIER_PIERCING_RETRIBUTION,
+
+	/// <summary>1st indicator</summary>
+	RAPIER_VERDADERA_DESTREZA_I,
+	/// <summary>2nd indicator</summary>
+	RAPIER_VERDADERA_DESTREZA_II,
+	/// <summary>3rd indicator</summary>
+	RAPIER_VERDADERA_DESTREZA_III,
+
+	/// <summary>Mid-air special attack</summary>
+	RAPIER_SILVER_CROSS,
+	/// <summary>Upgraded mid-air special attack</summary>
+	RAPIER_CROSS_OF_THE_TEMPEST,
+
+	/// <summary>Barrage of thrusts</summary>
+	RAPIER_SILVER_STORM,
+	/// <summary>Upgraded barrage of thrusts</summary>
+	RAPIER_SILVER_LIGHTNING,
+
+	/// <summary>Charged hit combo</summary>
+	RAPIER_SILVER_NAIL,
+
+	/* Mea Culpa attacks */
+
+	/// <summary>Dash attack</summary>
+	MEA_CULPA_SACRED_ONSLAUGHT,
+
+	/// <summary>Default parry</summary>
+	MEA_CULPA_THORN_GUARD,
+	/// <summary>Upgraded parry</summary>
+	MEA_CULPA_FERVOROUS_RETRIBUTION,
+
+	/// <summary>Projectile</summary>
+	MEA_CULPA_PERCEPTOR_PULSE,
+	/// <summary>Projectile piercing</summary>
+	MEA_CULPA_PATH_OF_DESTRUCTION,
+	/// <summary>Projectile explosion</summary>
+	MEA_CULPA_END_OF_LINE,
+
+	/// <summary>Default charged attack</summary>
+	MEA_CULPA_HOLY_WRATH,
+	/// <summary>Upgraded charged attack</summary>
+	MEA_CULPA_WRATH_OF_THE_TWISTED_ONE,
+
+	/// <summary>Default ground slam</summary>
+	MEA_CULPA_WEIGHT_OF_JUSTICE,
+	/// <summary>Upgraded ground slam</summary>
+	MEA_CULPA_CONSECRATION,
+
+	/// <summary>3rd combo ascension</summary>
+	MEA_CULPA_FERVOROUS_ASCENSION,
+	/// <summary>3rd combo cyclone</summary>
+	MEA_CULPA_FERVOROUS_CYCLONE,
+	/// <summary>4th combo</summary>
+	MEA_CULPA_LAST_WORDS,
+
+	/// <summary>Activation</summary>
+	MEA_CULPA_INCARNATE_REMORSE,
 }
 
--- BlasII.StylePoints/Bonuses/BonusesManager.cs
+++ BlasII.StylePoints/Bonuses/BonusesManager.cs
@@ -26,6 +26,91 @@ public class BonusesManager : Manager<BonusID, Bonus>
 	/// </summary>
 	protected override void Fill()
 	{
+		/* Common */
+
+		Add(BonusID.KILL, "Kill", 10);
+		Add(BonusID.BIG_KILL, "Big Kill", 50);
+		Add(BonusID.OVER_KILL, "Over Kill", 50);
+		Add(BonusID.EXECUTION, "Execution", 100);
+		Add(BonusID.BIG_EXECUTION, "Big Execution", 200);
+
+		/* Blade */
+
+		Add(BonusID.BLADE_ROSARY_WARD, "Rosary Ward", 20);
+		Add(BonusID.BLADE_RETRIBUTION, "Retribution", 30, Color.red);
+
+		Add(BonusID.BLADE_WEIGTH_OF_SIN, "Weight of Sin", 20);
+		Add(BonusID.BLADE_WEIGTH_OF_JUSTICE, "Weight of Justice", 30, Color.red);
+
+		Add(BonusID.BLADE_CRIMSON_ASCENSION, "Crimson Ascension", 20, Color.red);
+		Add(BonusID.BLADE_CRIMSON_CYCLONE, "Crimson Cyclone", 20, Color.red);
+
+		Add(BonusID.BLADE_CRIMSON_CLEAVER, "Crimson Cleaver", 100);
+		Add(BonusID.BLADE_CRIMSON_GLAIVE, "Crimson Glave", 150, Color.red);
+
+		Add(BonusID.BLADE_BLOOD_PACT, "Blood Pact", 10, Color.red);
+		Add(BonusID.BLADE_REAPER_ROSARY, "Reaper Rosary", 15, Color.red);
+
+		/* Censer */
+
+		Color orange = new (100, 50, 0);
+
+		Add(BonusID.CENSER_THUNDER_OF_MERCY, "Thunder of Mercy", 30);
+		Add(BonusID.CENSER_AZURE_TYPHOON, "Azure Typhoon", 30);
+		Add(BonusID.CENSER_AND_THE_EARTH_SHATTERED, "And the Earth Shattered", 40, orange);
+		Add(BonusID.CENSER_SOUL_COMBUSTION, "Soul Combustion", 50, orange);
+
+		Add(BonusID.CENSER_EMBERS_OF_FAITH, "Embers of Faith", 10, orange);
+		Add(BonusID.CENSER_EXPLOSIVE_EMBERS, "Explosive Embers", 15, orange);
+
+		Add(BonusID.CENSER_IGNITION_STRIKE, "Ignition Strike", 15, orange);
+		Add(BonusID.CENSER_AERIAL_IGNITION, "Aerial Ignition", 15, orange);
+		Add(BonusID.CENSER_ENDLESS_FLAME, "Endless Flame", 15, orange);
+
+		/* Sarmiento y Centella */
+
+		Add(BonusID.RAPIER_LUNGE, "Lunge", 10);
+		Add(BonusID.RAPIER_SILVER_LUNGE, "Silver Lunge", 10, Color.cyan);
+
+		Add(BonusID.RAPIER_CROSS_GUARD, "Cross Guard", 10);
+		Add(BonusID.RAPIER_PIERCING_RETRIBUTION, "Piercing Retribution", 10, Color.cyan);
+
+		Add(BonusID.RAPIER_VERDADERA_DESTREZA_I, "Verdadera Destreza I", 10, Color.cyan);
+		Add(BonusID.RAPIER_VERDADERA_DESTREZA_II, "Verdadera Destreza II", 20, Color.cyan);
+		Add(BonusID.RAPIER_VERDADERA_DESTREZA_III, "Verdadera Destreza III", 30, Color.cyan);
+
+		Add(BonusID.RAPIER_SILVER_CROSS, "Silver Cross", 10);
+		Add(BonusID.RAPIER_CROSS_OF_THE_TEMPEST, "Cross of the Tempest", 20, Color.cyan);
+
+		Add(BonusID.RAPIER_SILVER_STORM, "Silver Storm", 10);
+		Add(BonusID.RAPIER_SILVER_LIGHTNING, "Silver Lightning", 20, Color.cyan);
+
+		Add(BonusID.RAPIER_SILVER_NAIL, "Silver Nail", 15);
+
+		/* Mea Culpa */
+
+		Color thorns = Color.magenta;
+
+		Add(BonusID.MEA_CULPA_SACRED_ONSLAUGHT, "Sacred Onslaught", 15);
+
+		Add(BonusID.MEA_CULPA_THORN_GUARD, "Thorn Guard", 10);
+		Add(BonusID.MEA_CULPA_FERVOROUS_RETRIBUTION, "Fervorous Retribution", 20, thorns);
+
+		Add(BonusID.MEA_CULPA_PERCEPTOR_PULSE, "Perceptor Pulse", 10, thorns);
+		Add(BonusID.MEA_CULPA_PATH_OF_DESTRUCTION, "Path of Destruction", 15, thorns);
+		Add(BonusID.MEA_CULPA_END_OF_LINE, "End of Line", 15, thorns);
+
+		Add(BonusID.MEA_CULPA_HOLY_WRATH, "Holy Wrath", 20);
+		Add(BonusID.MEA_CULPA_WRATH_OF_THE_TWISTED_ONE, "Wrath of The Twisted One", 30, thorns);
+
+		Add(BonusID.MEA_CULPA_WEIGHT_OF_JUSTICE, "Weight of Justice", 15);
+		Add(BonusID.MEA_CULPA_CONSECRATION, "Consecration", 20, thorns);
+
+		Add(BonusID.MEA_CULPA_FERVOROUS_ASCENSION, "Fervorous Ascension", 10, thorns);
+		Add(BonusID.MEA_CULPA_FERVOROUS_CYCLONE, "Fervorous Cyclone", 10, thorns);
+		Add(BonusID.MEA_CULPA_LAST_WORDS, "Last Words", 20, thorns);
+
+		Add(BonusID.MEA_CULPA_INCARNATE_REMORSE, "Incarnate Remorse", 30, thorns);
 	}
 }
 
